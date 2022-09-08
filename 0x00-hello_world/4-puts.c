4429ada99d6517b17e2df2b5447ad131139e9833
Musa Muhammad Etudaye <musaawwaletudaye@gmail.com>
2022-09-08 15:59:15 -0700

    fix 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  *  * main - Prints string
  *   * Description: prints "\"Programming is like building a multilingual puzzle"
  *    * Return: 0
- *     */
+ *    
+ **/
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
