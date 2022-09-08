afcacc988a29c04ff77ae85f9628e045c09519ad
Musa Muhammad Etudaye <musaawwaletudaye@gmail.com>
2022-09-08 16:15:26 -0700

    task 4 fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
+Vi 4-puts.c
 #include <stdio.h>
+
 /**
- *  *main- Serves as the entry point for the program
- *    *Return: By default returns zero if no erros otherwise returns non-zero value.
- *     */
+ * *main - Print
+ * *
+ * *Return: Always 0 (success)
+ * */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
