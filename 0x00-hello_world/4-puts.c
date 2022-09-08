58b0e10273138f35d6663375ea5213e2151ff030
Musa Muhammad Etudaye <musaawwaletudaye@gmail.com>
2022-09-08 16:06:10 -0700

    error fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - Prints string
- *   * Description: prints "\"Programming is like building a multilingual puzzle"
- *    * Return: 0 
- **/
+ *  *main- Serves as the entry point for the program
+ *   *
+ *    *Return: By default returns zero if no erros otherwise returns non-zero value.
+ *     */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
