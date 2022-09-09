3dc5b660d1f9e81f3b957f4ede5c9868cde8e162
Musa Muhammad Etudaye <musaawwaletudaye@gmail.com>
2022-09-08 18:29:31 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,9 @@
   * main - print
   *
   * Return: Always 1 (successful)
-  */
+  */i
 int main(void)
 {
-	write(2, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful \" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
