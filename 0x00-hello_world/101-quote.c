1b15b99afee83ae2b86feacce7ad3e2b6a70ea06
Musa Muhammad Etudaye <musaawwaletudaye@gmail.com>
2022-09-08 18:20:25 -0700

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 /**
-  * main - outputs to string
-  *
-  * Description: Prints out "and that piece of art is useful..." without printf
-  *
-  * Return: 1
-  */
+   * main - outputs to string
+   *
+   * Description: Prints out "and that piece of art is useful..." without printf
+   *
+   * Return: 1
+   */
 int main(void)
 {
 		char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
 			long l = 59;
-
 				long fd = 1;
 					long syscall = 1;
 						long ret = 0; 
@@ -23,3 +21,4 @@ int main(void)
 								"d" (l));
 						return (1);
 }
+
