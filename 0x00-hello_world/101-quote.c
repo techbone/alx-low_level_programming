56cffb444bda5dcc37841a29c4cd71d059ea1cd5
Musa Muhammad Etudaye <musaawwaletudaye@gmail.com>
2022-09-09 11:35:52 -0700

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - print quote
  *
@@ -8,7 +7,6 @@
  *
  * Return: Always 1 (Success)
  */
-
 int main(void)
 {
 		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
