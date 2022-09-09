ecc2005da3132914a0df2990dd36bcc52432e998
Musa Muhammad Etudaye <musaawwaletudaye@gmail.com>
2022-09-08 18:06:38 -0700

    final fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,23 @@
-#include <unistd.h>
+#include <stdio.h>
 /**
- * main -  print
- * 
- * Return: Always 1 (successful)
- */
+  * main - outputs to string
+  *
+  * Description: Prints out "and that piece of art is useful..." without printf
+  *
+  * Return: 1
+  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
-	return (1);
+		char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+			long l = 59;
+				long fd = 1;
+					long syscall = 1;
+						long ret = 0; 
+						__asm__ ("syscall"
+								: "=a" (ret)
+								: "a" (syscall),
+								"D" (fd),
+								"S" (s),
+								"d" (l));
+						return (1);
 }
