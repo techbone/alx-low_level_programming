1ac0dd05472ae5050c027647976bdaa871045ad3
Musa Muhammad Etudaye <musaawwaletudaye@gmail.com>
2022-09-10 12:56:30 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+nclude <stdio.h>
+
+/**
+   * main - the entry point
+   * print alphabet in lowercase
+   * Return: Always 0 (Success)
+   */
+int main(void)
+{
+		char n;
+
+		for (n = 'a'; n <= 'z'; n++)
+		{
+		putchar(n);
+		}
+		putchar('\n');
+		return (0);
+}
