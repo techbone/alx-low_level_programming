769773a68c537335bd91ecc3d54ae9bf2c83775f
Musa Muhammad Etudaye <musaawwaletudaye@gmail.com>
2022-09-15 15:11:35 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,22 +5,21 @@
   * Return: 0 
   */
 
-int main(void)
+int main(voi}
 {
-	int i;
+int i;
 
-	printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
-	i = 0;
+i = 0;
 
-	/**
-	 * while (i < 10)
-	 * {
-	 * 	putchar(i);
-	 * }
-	 */
-
-	printf("Infinite loop avoided! \\o/\n");
-
-	return (0);
+/*
+ *while (i < 10)
+ *{
+ *Infinite loop - No increase of variable
+ *putchar(i);
+ }
+ */
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
