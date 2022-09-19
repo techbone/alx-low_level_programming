1001aae4bddbbf2bc2c9761c0e64ec69ac4a1d5b
Musa Muhammad Etudaye <musaawwaletudaye@gmail.com>
2022-09-19 15:55:18 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - prints a string, to stdout
+ * @str: value to be evaluate.
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != 0; c++)
+	{
+		putchar(str[c]);
+	}
+	putchar('\n');
+}
