2a3c40987d256175d188c669204040bb52816434
Musa Muhammad Etudaye <musaawwaletudaye@gmail.com>
2022-09-23 16:01:37 -0700

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,14 +8,13 @@
  * @size_r: buffer size
  * Return: address of r or 0
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j, k, l, m, n;
 
-	for (i = 0; n1[i]; i++);
+	for (i = 0; n1[i]; i++)
 
-	for (j = 0; n2[j]; j++);
+	for (j = 0; n2[j]; j++)
 
 	if (i > size_r || j > size_r)
 		return (0);
